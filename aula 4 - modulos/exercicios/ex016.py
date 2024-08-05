e6e0b540c523505e78fede8ff050bcede3ea9c4c
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-04 21:25:52 -0300

    adicionando exercicio 16 de Python

---
+++ aula 4 - modulos/exercicios/ex016.py
@@ -0,0 +1,9 @@
+# crie um programa que recebe um numero com casas decimais e retorna ele inteiro
+
+import math
+
+num = float(input('Digite um numero '))
+
+roundDown = math.floor(num)
+
+print('O inteiro do numero {} é: {}!'.format(num, roundDown))

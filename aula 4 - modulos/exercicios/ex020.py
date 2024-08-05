4ffa76660c04a64e8ebce726ef870551e0c1f8d8
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-04 22:26:07 -0300

    adicionando exerecicio 20 de python

---
+++ aula 4 - modulos/exercicios/ex020.py
@@ -0,0 +1,8 @@
+# crie um algoritimo que recebe strings e depois mostra elas na funçao print
+import random
+
+students = input('Qual nome dos alunos? digite separando os nomes por espaços ')
+
+list = students.split()
+
+print(list)

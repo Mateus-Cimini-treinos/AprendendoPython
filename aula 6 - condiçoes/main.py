db6cad3052924fd51bf9ef9671ff354798d138a5
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-05 19:32:22 -0300

    adicionando aula 6 de condiçonais

---
+++ aula 6 - condiçoes/main.py
@@ -0,0 +1,10 @@
+# aula 6 de python sobre condiçoes if,elif e else
+
+carYears = int(input('Quantos anos tem o seu carro? '))
+
+if  carYears > 10 :
+     print('Seu carro e velho') 
+else : 
+     print('Seu carro e novo')
+
+print('--FIM--')

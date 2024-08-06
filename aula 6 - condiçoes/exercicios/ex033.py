365b6acfa94b85a01c20d0c5132be8728c0fef87
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-05 21:01:57 -0300

    adicionando exercicio 33 de python

---
+++ aula 6 - condiçoes/exercicios/ex033.py
@@ -0,0 +1,32 @@
+#crie um algoritimo que receba 3 numeros e fala qual e maior ou qual e menor
+
+num1 = 15
+num2 = 16
+num3 = 15
+
+
+if num1 >= num2 and num1 >= num3 :
+    print(' O maior numero é {}'.format(num1))
+elif num2 >= num1 and num2 >= num3 :
+    print('O maior numero é: {}'.format(num2))
+else :
+    print('O numero maior é {}'.format(num3))
+
+if num1 <= num2 and num1 <= num3 :
+    print('O menor numero é {}'.format(num1))
+elif num2 <= num1 and num2 <= num3 :
+    print('O menor numero é {}'.format(num2))
+else :
+    print('O menor numero é {}'.format(num3))
+
+
+
+
+
+
+
+
+'''num1 = int(input('Digite um numero'))
+num2 = int(input('Digite um numero'))
+num3 = int(input('Digite um numero'))'''
+

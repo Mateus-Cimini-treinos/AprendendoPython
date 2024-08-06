ae2bd97de9f23bf826bb429de7e97998b972678a
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-05 21:18:36 -0300

    adicionando exercicio 34 de python

--- aula 6 - condiçoes/exercicios/ex034.py
+++ aula 6 - condiçoes/exercicios/ex034.py
@@ -1,1 +1,15 @@
-#crie um algoritimo que
+#crie um algoritimo que recebe o valor do salario do fucionario e se o mo salario for menor que 1250 recebe um aumento de 15% e se for maior que 1250 recebe um aumento de 10%
+
+wage = float(input('Qual o salario do Fucionario? R$'))
+
+increase1 = 10
+increase2 = 15
+
+if wage > 1250 :
+    increase = wage * (1 + increase1 / 100)
+    print('O salario era de {} mais os 10%\n foi para {:.2f}'.format(wage, increase))
+
+else :
+    increase = wage * (1 + increase2 / 100)
+    print('O salario era de {} mais os 15%\n foi para {:.2f}'.format(wage, increase))
+             

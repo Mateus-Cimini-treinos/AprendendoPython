59b183685249ba97d00ee15ea1945dd84835d565
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-06 21:29:48 -0300

    Adicionando exercicio 47 de python

--- aula 9 - Estrutura de repetiçao (for)/exercicios/ex47.py
+++ aula 9 - Estrutura de repetiçao (for)/exercicios/ex47.py
@@ -1,5 +1,5 @@
 # crie um programa que mostre todos os numeros pares de 1 a 50
 
-for n in range(0,51,2):
+for n in range(2,51,2):
     print(n)
 print("fim")
---
+++ aula 9 - Estrutura de repetiçao (for)/exercicios/ex48.py
@@ -0,0 +1,7 @@
+#crie um programa que some todos os numeros inpares multiplos de 3 que se encontran entre 1 e 500
+s = 0
+for n in range(0, 500, 3):  
+    s += n
+print(s)
+print('fim')
+

f590c2492879d0204c87d43ab89d3f8e47f9ee1f
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-06 21:03:57 -0300

    Adicionando exercicio 46 de python

---
+++ aula 9 - Estrutura de repetiçao (for)/exercicios/ex46.py
@@ -0,0 +1,9 @@
+# crie um algoritimo que faz uma contagem regresiva de 5 segundos ate o ano novo!!
+
+import time
+
+for n in range(5, -1, -1):
+    print(n)
+    time.sleep(1)
+print("FELIZ ANO NOVO")
+

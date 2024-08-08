44f1cd8e3437c0c042582f388092a4f17cc49d52
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-08 13:53:34 -0300

    Adiciondo exercicio 64 de python

---
+++ aula 10 - Estrutura de repetiçao (while)/exercicios/ex64.py
@@ -0,0 +1,14 @@
+# crie um programa que recebe numeros ate que a resposta seja 999 e no final mostre quantos numeros foi digitados e a soma entre todos numeros digitados
+
+num = 0
+soma = 0
+cont = 1
+while num != 999 :
+    num = int(input('qual vai ser o numero da casa {}?? '.format(cont)))
+    ultimo = num
+    if ultimo == 999 :
+        soma -= 999
+        cont -= 2
+    cont += 1
+    soma += num
+print("voce digitou {} numeros a soma de todos os numeros e de : {}".format(cont,soma))

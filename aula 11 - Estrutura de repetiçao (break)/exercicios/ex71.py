3c2b7f2c4af40fa7c538555686ded85bfffde276
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-08 17:12:51 -0300

    Adiciondo exercicio 71 de python

---
+++ aula 11 - Estrutura de repetiçao (break)/exercicios/ex71.py
@@ -0,0 +1,20 @@
+
+valor = int(input('qual o valor vc vai sacar?'))
+total = valor 
+ced = 50
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        print(f'Total de {totalced} cedulas de R$ {ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totalced = 0
+        if total == 0:
+            break

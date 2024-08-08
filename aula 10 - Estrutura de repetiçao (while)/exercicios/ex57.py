275d0c77c64e35915a4c878296a8dc33cddba93e
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-07 21:04:10 -0300

    Adiciondo exercicio 57 de python

---
+++ aula 10 - Estrutura de repetiçao (while)/exercicios/ex57.py
@@ -0,0 +1,7 @@
+# crie um algoritimo que pergunta o sexo da pessoa e so aceita resposta com M ou F e se a pessoa resondeer outra coisa repete a pergunta dnv
+
+sexo = 'C'
+while sexo != 'M' and  sexo != 'F':
+    sexo = str(input('Qual o seu sexo? [M/F]')).upper()
+print('Acabou')
+

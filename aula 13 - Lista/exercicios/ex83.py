50a96204d1ead1848d8e33d16c1c24720b615bcd
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-10 15:27:10 -0300

    Adiciondo exercicio 83 de python

--- aula 13 - Lista/exercicios/ex83.py
+++ aula 13 - Lista/exercicios/ex83.py
@@ -1,1 +1,15 @@
-num
+exp = str(input('Digite uma expressao'))
+pilha = []
+for simb in exp:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('deu certo')
+else:
+    print('deu erro')

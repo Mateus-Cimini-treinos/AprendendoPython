b66907fb165fd41071f1e0220d7b8449704cfb77
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-10 14:42:15 -0300

    Adiciondo exercicio 81 de python

--- aula 13 - Lista/exercicios/ex81.py
+++ aula 13 - Lista/exercicios/ex81.py
@@ -1,1 +1,26 @@
-#crie um programa que
+#crie um programa que vai ler numeros do usuario enquanto ele quiser e no final mostrar
+# quantos numeros foram digitados
+#ordem ordenada de forma decrecente
+# se o valor 5 foi digitado
+
+num = []
+while True:
+    num.append(int(input('Adicione um valor à lista: ')))
+    continuar = input('Quer adicionar mais números? [S/N] ').strip().upper()
+    if continuar == 'N':
+        break
+    elif continuar != 'S':
+        print('Resposta inválida! Digite S para sim ou N para não.')
+
+quantidade = len(num)
+
+print(f'Os números adicionados são: {num}')
+print(f'Você adicionou {quantidade} números')
+print(f'Seus números em ordem decrescente: {sorted(num, reverse=True)}')
+
+if 5 in num:
+    print('O número 5 está na lista')
+    posicao = num.index(5)
+    print(f'E o número 5 está na posição {posicao}')
+else:
+    print('Não há 5 na lista')

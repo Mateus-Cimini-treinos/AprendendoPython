c9ca4ad18038a0190bc2d6f2ff54834f14782426
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-10 14:16:30 -0300

    Adiciondo exercicio 80 de python

--- aula 13 - Lista/exercicios/ex79.py
+++ aula 13 - Lista/exercicios/ex79.py
@@ -1,1 +1,20 @@
-#crie um programa que
+#crie um programa que recebe valores ate o usuario dizer nao, e esses valores sao cadastrados em uma lista e se o numero e repetido ele nao e salvo, e no final mostra os valores em ordem cressente
+
+
+continuar = 'S'
+num = []
+numUnicos = []
+
+while True:
+    num.append(int(input('Adicione um valor à lista: ')))
+    continuar = input('Quer adicionar mais números? [S/N] ').strip().upper()
+    if continuar == 'N':
+        break
+    elif continuar != 'S':
+        print('Resposta inválida! Digite S para sim ou N para não.')
+
+for n in num:
+    if n not in numUnicos:
+        numUnicos.append(n)
+
+print(f'A lista principal é {num}, mas a lista com os repetidos removidos é {numUnicos}.')
--- aula 13 - Lista/exercicios/ex80.py
+++ aula 13 - Lista/exercicios/ex80.py
@@ -1,1 +1,17 @@
-#crie um programa que
+#crie um programa que o usuario deve cadastrar 5 numeros e o programa deve ja cadastralos na posiçao correta sem usar sort
+
+lista = []
+for c in range(0,5):
+    n = int(input('digite um valor '))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+        print('valor foi adicionado no final da lista')
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                print(f'Adicionado na posiçao {pos} da lista')
+                break
+            pos += 1
+print(f'os valores adicionados sao {lista}')

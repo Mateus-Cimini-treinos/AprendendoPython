021a1bce95e173d4ab3fd7c193f3fe84ddda42b2
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-17 15:59:02 -0300

    Adicionando aula sobre Typecasting

---
+++ aula 20 - Typecasting/main.py
@@ -0,0 +1,9 @@
+# aula sobre Typecasting que a conversao de tipo de variavel por exemplo uma variavel string para int 
+
+numStr = '123'
+numInt = int(numStr) # Converte string para inteiro
+
+numFloat = float(numStr) # Converte a string para float
+
+num_str = str(2121) #Converte inteiro para string
+

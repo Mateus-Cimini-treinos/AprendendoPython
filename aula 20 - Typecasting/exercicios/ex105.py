fa1ddcf25a357b2b2bc049cff47bbcf6a54c29d4
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-08-17 16:40:50 -0300

    Adicionando exercicio 05 sobre Typecasting

---
+++ aula 20 - Typecasting/exercicios/ex105.py
@@ -0,0 +1,4 @@
+# Peça ao usuário para digitar uma frase. Converta a string em uma lista de palavras e exiba a lista.
+frase = input('Digite um frase ')
+partes = frase.split()
+print(f' sua lista ficou assim {partes}')
